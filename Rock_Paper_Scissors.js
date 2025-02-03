915edac472c62552d7734db36df174dc800f70cf
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2025-02-03 21:11:59 +0900

    feat: 가위바위보 게임 - 컴퓨터 선택 함수 구현
     - getComputerChoice 함수 구현: 랜덤으로 '가위', '바위', '보' 중 하나를 반환
     - console.log를 통해 함수 결과 확인

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -1,1 +1,12 @@
-console.log("HelloWorld!");
+function getComputerChoice() {
+  let randomly = Math.floor(Math.random() * 3) + 1;
+  if (randomly == 1) {
+    return "가위";
+  } else if (randomly == 2) {
+    return "바위";
+  } else {
+    return "보";
+  }
+}
+console.log(getComputerChoice());
+function getHumanChoice() {}
