1e1fac763701c31b94d760ce45a5ad1059db14a3
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2025-02-04 19:52:14 +0900

    feat: 사용자 선택 함수(getHumanChoice) 구현
     - prompt를 통해 사용자 입력(1: 가위, 2: 바위, 3: 보)을 받고, 숫자로 변환
     -  입력값에 따라 '가위', '바위', '보'를 반환하는 getHumanChoice 함수 구현
     -  유효하지 않은 입력의 경우 "undefined"를 출력하도록 처리

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -4,9 +4,24 @@ function getComputerChoice() {
     return "가위";
   } else if (randomly == 2) {
     return "바위";
-  } else {
+  } else if (randomly == 3) {
     return "보";
+  } else {
+    console.log("undefined");
   }
 }
 console.log(getComputerChoice());
-function getHumanChoice() {}
+let user = Number(prompt("원하는 것을 내세요 : 1 가위 : 2 바위 : 3 보"));
+
+function getHumanChoice() {
+  if (user == 1) {
+    return "가위";
+  } else if (user == 2) {
+    return "바위";
+  } else if (user == 3) {
+    return "보";
+  } else {
+    console.log("undefined");
+  }
+}
+console.log(getHumanChoice(user));
